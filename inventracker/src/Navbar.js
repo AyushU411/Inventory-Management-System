606e53eee12afd7bed5204f6a4450ebd63bd2fb6
Ayush Upadhyay <ayushhupadhyay@gmail.com>
2024-07-08 03:19:51 +0530

    Started Creating the Frontend.

---
+++ inventracker/src/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Navbar(){
+    return(
+        <div className="nav">
+            <div className="left">invenTracker  </div>
+            <div className="right"><pre><a>Home </a>         <a>Option1</a>          <a>Option2</a></pre></div>
+            <div className="navlogin">Login/Signup </div>
+        </div>
+    )
+}
+
+export default Navbar;
